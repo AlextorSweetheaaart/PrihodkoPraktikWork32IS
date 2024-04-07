68e786a0c8dd4bcd9051fc6711eb4f74c15eace5
Alextor <sashafoxalex.2.0@gmail.com>
2024-04-07 17:06:08 +0300

    Практическая номер 1 Не система

---
+++ prihodkoDidIt1/prihodkoDidIt1/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prihodkoDidIt1.Models
+{
+    public class Book
+    {
+        public string Autor { get; set; }
+
+        public string Name {  get; set; }
+
+        public int Pages {  get; set; }
+
+        //Что-то важное в вмде того - откуда будут браться данные
+    }
+}
---
+++ prihodkoDidIt1/prihodkoDidIt1/ViewModels/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using prihodkoDidIt1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prihodkoDidIt1.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Book _book;
+        // Всё в один объект
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        //Ту вообще магия вне хогвартса
+        public string Autor
+        {
+            get { return _book.Autor; } //Это типа метод для вызова 
+            
+            set
+            {
+                if (_book.Autor != value) //Если не равно значению, то смена значения
+                {
+                    _book.Autor = value;
+                    OnPropertyChanged(nameof(Autor));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return _book.Name; } //Это типа метод для вызова 
+
+            set
+            {
+                if (_book.Name != value) //Если не равно значению, то смена значения
+                {
+                    _book.Name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public int Pages
+        {
+            get { return _book.Pages; } //Это типа метод для вызова 
+
+            set
+            {
+                if (_book.Pages != value) //Если не равно значению, то смена значения
+                {
+                    _book.Pages = value;
+                    OnPropertyChanged(nameof(Pages));
+                }
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            _book = new Book();
+
+            //Создание объекта 
+        }
+    }
+}
